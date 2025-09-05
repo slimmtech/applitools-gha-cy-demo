cc849604a90c4b43e614b4a8f93fc0a8da4f6a3d
Mike Millgate <mike.millgate@applitools.com>
2025-09-05 11:52:31 -0600

    adding forced failure logic

--- cypress/e2e/applitools-demo_test.cy.js
+++ cypress/e2e/applitools-demo_test.cy.js
@@ -1,7 +1,16 @@
 describe("Applitools Demo Test", () => {
   it("Visits the Applitools demo page and performs visual testing", () => {
     // Visit the Applitools demo page
-    cy.visit("helloworld");
+    if (
+      Cypress.env("FORCED_FAILURE") === false ||
+      Cypress.env("FORCED_FAILURE") === undefined
+    ) {
+      cy.visit("helloworld");
+    } else {
+      cy.log("Forcing a visual failure");
+      console.log("Forcing a visual failure");
+      cy.visit("helloworld?diff1");
+    }
 
     // Perform visual testing using Applitools
     cy.eyesOpen({
